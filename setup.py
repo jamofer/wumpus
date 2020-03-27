1828d53ecb8cd8d480f61e42f931fb728d433a45
javier.moragon <jamofer@gmail.com>
2020-03-27 04:19:51 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wumpus',
+    version='',
+    packages=['wumpus', 'wumpus.game', 'wumpus.entities'],
+    url='',
+    license='',
+    author='Javier Moragón',
+    author_email='',
+    description=''
+)
